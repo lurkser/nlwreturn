d602e489a3b4008cd697fff1018c5b939d6c7209
Lucas Paiva <lucaspaivax6@protonmail.com>
2022-05-06 16:58:26 -0300

    adicionada variável port pro railway

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,12 @@ import express from 'express'
 import { routes } from './routes'
 import cors from 'cors'
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(routes)
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log('HTTP server rodando')
-})
+});
